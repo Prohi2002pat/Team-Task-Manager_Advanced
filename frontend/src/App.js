d293b55679f5568b2bc1e66978d1652e78af4561
ROHIT SATISH PATIL <126314851+Prohi2002pat@users.noreply.github.com>
2026-05-06 20:39:47 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,3 @@
-// import { useState } from "react";
-// import Login from "./pages/Login";
-// import Dashboard from "./pages/Dashboard";
-// import CreateTask from "./pages/CreateTask";
-
-// function App() {
-//   const [isLoggedIn, setIsLoggedIn] = useState(
-//     !!localStorage.getItem("token")
-//   );
-
-//   return (
-//     <div>
-//       {isLoggedIn ? (
-//         <>
-//           <CreateTask />
-//           <Dashboard />
-//         </>
-//       ) : (
-//         <Login setIsLoggedIn={setIsLoggedIn} />
-//       )}
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
